5a2208f8d555376f5c7bd90e36a1fc535919c2af
ryba147 <54861049+ryba147@users.noreply.github.com>
2021-03-09 22:06:57 +0200

    Login page

---
+++ js/login.js
@@ -0,0 +1,24 @@
+var url = "http://flask-lpnu.herokuapp.com/user/login/";
+var data = {username: "taras", password: "admin"};
+var datatype = 'jsonp';
+
+$(document).ready(function () {
+    $('form').on('submit', function (event) {
+        $.ajax({
+            data: {
+                username: $('#usernameInput').val(),
+                password: $('#passwordInput').val()
+            },
+            type: 'GET',
+            url
+        }).done(function (data) {
+            if (data.error) {
+                alert("Denied");
+            }
+            else {
+                alert("Approved");
+            }
+        });
+        event.preventDefault();
+    });
+});
