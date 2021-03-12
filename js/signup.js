84eccc1bb2d13e21a22ba57da120d90dc736221a
ryba147 <54861049+ryba147@users.noreply.github.com>
2021-03-12 21:47:12 +0200

    Working sign-up. Separate file

--- js/signup.js
+++ js/signup.js
@@ -1,31 +1,51 @@
-var url = "https://flask-lpnu.herokuapp.com/user/";
-var datatype = 'jsonp';
+window.onload = function() {
+    const apiUrl = "https://flask-lpnu.herokuapp.com/user/";
+    var logregForm = document.getElementById("signup-form");
 
-$(document).ready(function () {
-    $('form').on('submit', function (event) {
-        $.ajax({
-            url: "https://flask-lpnu.herokuapp.com/user/",
-            type: "POST",
-            data:
-                // $('form').serialize(),
-            JSON.stringify({
-                "username": "taras1",
-                "email": "taras1@taras.com",
-                "firstname": "Taras",
-                "lastname": "Vilinskyi",
-                "password": "admin1",
-                "location": 123
-            }),
-            contentType: "application/json; charset=utf-8",
-            datatype: datatype,
-        }).done(function (data) {
-            console.log(data);
-            if (!data.error) {
-                alert("Denied");
-            } else {
-                alert("success")
-            }
-        });
+    logregForm.onsubmit = function (event) {
         event.preventDefault();
-    });
-});
+
+        var request = new XMLHttpRequest(); // xhr
+
+        var firstName = encodeURIComponent(document.getElementById("firstnameInput").value);
+        var lastName = encodeURIComponent(document.getElementById("lastnameInput").value);
+        var email = encodeURIComponent(document.getElementById("emailInput").value);
+        var username = encodeURIComponent(document.getElementById("usernameInput").value);
+        var password = encodeURIComponent(document.getElementById("password").value);
+
+        var radios = document.getElementsByName("roleInput");
+        for (let i = 0, length = radios.length; i < length; ++i) {
+            if (radios[i].checked) {
+                // alert(radios[i].value);
+                var userRole = radios[i].value;
+
+                break; // якщо знайшли обране - виходимо з циклу
+            }
+        }
+
+        request.open("POST", apiUrl, false);
+
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({
+            "firstname": firstName,
+            "lastname": lastName,
+            "email": email,
+            "username": username,
+            "role": userRole,
+            "password": password
+        }));
+        console.log(request.response);
+
+        if (request.status === 201) {
+            alert("User created");
+            location.href = "/login.html";
+        }
+        else if (request.status === 403) {
+            alert("User already exists");
+            location.href = "/login.html";
+        }
+        else  {
+            alert("An error occurred while creating user");
+        }
+    }
+}
