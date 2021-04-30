e87877b984c56fb2f39c3b32bea58ca06c5e9818
ryba147 <54861049+ryba147@users.noreply.github.com>
2021-04-30 11:33:20 +0300

    Registration component

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,17 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './page/login/login.component';
-import { RegisterComponent } from './page/register/register.component';
-
+import { RegistrationComponent } from './page/registration/registration.component';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [LoginComponent, RegistrationComponent],
+  imports: [CommonModule, AuthRoutingModule],
 })
-export class AuthModule { }
+export class AuthModule {}
