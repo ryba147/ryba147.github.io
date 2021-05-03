b93d57931ddbef4f159b63b293bc30bf3a0d2d66
ryba147 <54861049+ryba147@users.noreply.github.com>
2021-05-03 15:46:11 +0300

    Reactive forms validation

--- src/app/modules/auth/page/registration/registration.component.ts
+++ src/app/modules/auth/page/registration/registration.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '@core/services/user.service';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Router} from '@angular/router';
-import {HttpErrorResponse} from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Constants } from '@shared/constants';
 
 @Component({
   selector: 'app-registration',
@@ -18,6 +19,10 @@ export class RegistrationComponent implements OnInit {
     this.buildForm();
   }
 
+  get f() {
+    return this.regForm.controls;
+  }
+
   ngOnInit(): void {
   }
 
@@ -26,22 +31,33 @@ export class RegistrationComponent implements OnInit {
       firstname: ['', [Validators.required]],
       lastname: ['', [Validators.required]],
       username: ['', [Validators.required]],
-      email: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.pattern(Constants.EMAIL_REGEX)]],
       role: [this.userRole, [Validators.required]],
       password: ['', [Validators.required]],
       confirmPassword: ['', [Validators.required]],
-    });
-
+    }, { validators: this.passwordMatcher });
   }
 
   changeRole($event: any): void {
     // console.log($event.target.value);
     this.userRole = $event.target.value;
+    this.regForm.controls.role.setValue(this.userRole);
+  }
+
+  passwordMatcher(formGroup: FormGroup): boolean | { noMatch: boolean } {
+    const password = formGroup.get('password').value;
+    const confirmPassword = formGroup.get('confirmPassword').value;
+
+    return password == confirmPassword ? null : { noMatch: true };
   }
 
   signup(): void {
+    if (this.regForm.invalid) {
+      return;
+    }
+
     this.userService.createUser(this.regForm.value).subscribe((response) => {
-      // console.log(response);
+      console.log(response);
       this.router.navigate(['login']);
       },
       (error: HttpErrorResponse) => {
--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,4 +1,5 @@
 export class Constants {
   public static API_URL = 'http://127.0.0.1:5000/';
   public static CLOUDINARY_URL = 'https://res.cloudinary.com/htmpcvv0s/image/upload/';
+  public static EMAIL_REGEX = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 }
