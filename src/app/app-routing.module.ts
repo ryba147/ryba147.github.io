6a41b1229d1a2a33ad2cf5781ea26fb59152126c
ryba147 <54861049+ryba147@users.noreply.github.com>
2021-05-03 16:02:10 +0300

    Transfering userlist to homecomponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from '@modules/home/page/home/home.component';
-import { UserProfileComponent } from '@modules/user-profile/page/user-profile/user-profile.component';
-import { AuthGuard } from '@core/guards/auth.guard';
-import {LoginComponent} from '@modules/auth/page/login/login.component';
-import {RegistrationComponent} from '@modules/auth/page/registration/registration.component';
 
 const routes: Routes = [
 
